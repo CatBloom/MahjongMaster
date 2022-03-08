5cf5ff788e2805d2f0d797002c2c332525030d45
CatBloom <shomail.cat@gmail.com>
2022-03-08 17:07:36 +0900

    Add:AngulerTemplate set

---
+++ src/app/app.module.ts
@@ -0,0 +1,28 @@
+// modules
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { AppRoutingModule } from './app-routing.module';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
+import { MaterialModule } from './material.module';
+
+// components
+import { AppComponent } from './app.component';
+import { HeaderComponent } from './shared/components/header/header.component';
+
+@NgModule({
+  declarations: [AppComponent, HeaderComponent],
+  imports: [
+    BrowserModule,
+    BrowserAnimationsModule,
+    AppRoutingModule,
+    FormsModule,
+    ReactiveFormsModule,
+    HttpClientModule,
+    MaterialModule,
+  ],
+  providers: [],
+  bootstrap: [AppComponent],
+})
+export class AppModule {}
