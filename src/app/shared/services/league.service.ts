5cc4ed21dc88b1922979e81b9442a8b22d60e182
CatBloom <shomail.cat@gmail.com>
2022-03-09 00:57:15 +0900

    Add:新規大会を登録する関数を作成 #2

--- src/app/shared/services/league.service.ts
+++ src/app/shared/services/league.service.ts
@@ -1,8 +1,16 @@
 import { Injectable } from '@angular/core';
+import { HttpClient, HttpHeaders, HttpErrorResponse } from '@angular/common/http';
+import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root',
 })
 export class LeagueService {
-  constructor() {}
+  private readonly expressUrl = 'http://localhost:3000'; //バックエンドにアクセスするURL
+  constructor(private http: HttpClient) {}
+
+  //サーバーに大会を登録
+  postLeague(leagueName: string): Observable<void> {
+    return this.http.post<void>(`${this.expressUrl}/league`, leagueName).pipe();
+  }
 }
