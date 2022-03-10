b9a5610333a36c74928a85af23619153b7ae4112
CatBloom <shomail.cat@gmail.com>
2022-03-10 22:44:25 +0900

    Add:基本ルール用のラジオボタンを設置 #4

--- src/app/rules/rules.component.ts
+++ src/app/rules/rules.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-rules',
@@ -7,6 +8,11 @@ import { Component, OnInit } from '@angular/core';
 })
 export class RulesComponent implements OnInit {
   constructor() {}
-
+  formGroup = new FormGroup({
+    radioGame: new FormControl('', [Validators.required]),
+    radioDora: new FormControl('', [Validators.required]),
+    radioTanyao: new FormControl('', [Validators.required]),
+    radioTime: new FormControl('', [Validators.required]),
+  });
   ngOnInit(): void {}
 }
