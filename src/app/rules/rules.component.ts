6f5db8e7de3532786852daeac46cf37d88511dea
CatBloom <shomail.cat@gmail.com>
2022-03-10 23:03:57 +0900

    Add:既存基本ルールを記載 #4

--- src/app/rules/rules.component.ts
+++ src/app/rules/rules.component.ts
@@ -1,12 +1,26 @@
 import { Component, OnInit } from '@angular/core';
 import { FormControl, FormGroup, Validators } from '@angular/forms';
-
+import { Rules } from '../shared/interfaces/rules';
 @Component({
   selector: 'app-rules',
   templateUrl: './rules.component.html',
   styleUrls: ['./rules.component.scss'],
 })
 export class RulesComponent implements OnInit {
+  // 雀魂公式ルール
+  readonly mahjongsoulRules: Rules = {
+    radioGame: '2',
+    radioDora: '2',
+    radioTanyao: '2',
+    radioTime: '3',
+  };
+  // 天鳳公式ルール
+  readonly tenhouRules: Rules = {
+    radioGame: '2',
+    radioDora: '2',
+    radioTanyao: '2',
+    radioTime: '2',
+  };
   constructor() {}
   formGroup = new FormGroup({
     radioGame: new FormControl('', [Validators.required]),
