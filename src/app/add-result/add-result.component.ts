4fe6127a942dd34fda84da5233ed6b7725ec3012
CatBloom <shomail.cat@gmail.com>
2022-03-17 22:03:34 +0900

    Add:Player名Inputの追加 #5

--- src/app/add-result/add-result.component.ts
+++ src/app/add-result/add-result.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-add-result',
@@ -6,6 +7,12 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./add-result.component.scss'],
 })
 export class AddResultComponent implements OnInit {
+  formGroup = new FormGroup({
+    playerName1: new FormControl('', [Validators.required]),
+    playerName2: new FormControl('', [Validators.required]),
+    playerName3: new FormControl('', [Validators.required]),
+    playerName4: new FormControl('', [Validators.required]),
+  });
   constructor() {}
 
   ngOnInit(): void {}
