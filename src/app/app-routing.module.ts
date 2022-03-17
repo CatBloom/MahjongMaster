ab30d0023b44ece81792a88e3571395ba789ed7c
CatBloom <shomail.cat@gmail.com>
2022-03-17 21:56:42 +0900

    Add:Routingに追加 #5

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { AddLeagueComponent } from './add-league/add-league.component';
+import { AddResultComponent } from './add-result/add-result.component';
 
 const routes: Routes = [
   { path: '', redirectTo: '', pathMatch: 'full' },
   { path: 'add-league', component: AddLeagueComponent },
+  { path: 'add-result', component: AddResultComponent },
 ];
 
 @NgModule({
