6070296b84eb4b111059104a501d43d44ae11e88
CatBloom <shomail.cat@gmail.com>
2022-03-22 22:59:27 +0900

    Add:メッセージを受け取る関数を作成 #8

--- src/app/shared/services/snack.service.ts
+++ src/app/shared/services/snack.service.ts
@@ -1,8 +1,18 @@
 import { Injectable } from '@angular/core';
+import { MatSnackBar, MatSnackBarHorizontalPosition, MatSnackBarVerticalPosition } from '@angular/material/snack-bar';
 
 @Injectable({
   providedIn: 'root',
 })
 export class SnackService {
-  constructor() {}
+  horizontalPosition: MatSnackBarHorizontalPosition = 'end';
+  verticalPosition: MatSnackBarVerticalPosition = 'top';
+  constructor(private snackBer: MatSnackBar) {}
+
+  openSnackBer(message: string, action: string) {
+    this.snackBer.open(message, action, {
+      horizontalPosition: this.horizontalPosition,
+      verticalPosition: this.verticalPosition,
+    });
+  }
 }
