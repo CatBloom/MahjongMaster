324b174a3df7c8ebbe4aa0975eff3602d031a01b
CatBloom <shomail.cat@gmail.com>
2022-03-27 02:46:23 +0900

    Add:全角数字→半角数字に変換し数字以外を省く #11

--- src/app/shared/directives/replace.directive.ts
+++ src/app/shared/directives/replace.directive.ts
@@ -1,17 +1,50 @@
-import { Directive, Input, OnInit } from '@angular/core';
+import { Directive, Input, OnInit, OnDestroy } from '@angular/core';
 import { AbstractControl } from '@angular/forms';
+import { Subscription } from 'rxjs';
 
 @Directive({
   selector: '[appReplace]',
 })
-export class ReplaceDirective implements OnInit {
+export class ReplaceDirective implements OnInit, OnDestroy {
   @Input() appReplace!: AbstractControl | null;
 
   private formControl!: AbstractControl | null;
+  private subscriptions = new Subscription();
 
   constructor() {}
 
-  ngOnInit() {
+  ngOnInit(): void {
     this.formControl = this.appReplace;
+    this.replaceHulfNumber();
+  }
+
+  ngOnDestroy(): void {
+    this.subscriptions.unsubscribe();
+  }
+
+  private replaceHulfNumber(): void {
+    if (!this.formControl) {
+      return;
+    }
+    this.subscriptions.add(
+      this.formControl.valueChanges.subscribe(() => {
+        if (!this.formControl) {
+          return;
+        }
+        const value: string = this.formControl.value;
+        let newValue: string = value;
+
+        // 全角数字を半角数字に変換
+        newValue = newValue.replace(/[０-９]/g, (s) => {
+          return String.fromCharCode(s.charCodeAt(0) - 0xfee0);
+        });
+        // 数字以外の文字を空文字に変換
+        newValue = newValue.replace(/[^0-9]/g, '');
+
+        if (value !== newValue) {
+          this.formControl.setValue(newValue);
+        }
+      })
+    );
   }
 }
