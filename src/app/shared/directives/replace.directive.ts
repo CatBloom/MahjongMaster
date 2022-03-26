a10293783a088a6ad8ee89a58f9f5a07a529808e
CatBloom <shomail.cat@gmail.com>
2022-03-27 02:44:16 +0900

    Add:フォームからコントロールを受け取る #11

--- src/app/shared/directives/replace.directive.ts
+++ src/app/shared/directives/replace.directive.ts
@@ -1,8 +1,17 @@
-import { Directive } from '@angular/core';
+import { Directive, Input, OnInit } from '@angular/core';
+import { AbstractControl } from '@angular/forms';
 
 @Directive({
   selector: '[appReplace]',
 })
-export class ReplaceDirective {
+export class ReplaceDirective implements OnInit {
+  @Input() appReplace!: AbstractControl | null;
+
+  private formControl!: AbstractControl | null;
+
   constructor() {}
+
+  ngOnInit() {
+    this.formControl = this.appReplace;
+  }
 }
