68d1eccac45f60a0a4abaadc42ce46ed2e5ed64a
CatBloom <shomail.cat@gmail.com>
2022-03-31 21:33:13 +0900

    Add:Routingに追加 #23

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,11 +2,13 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { AddLeagueComponent } from './add-league/add-league.component';
 import { AddResultComponent } from './add-result/add-result.component';
+import { PlayerDetailsComponent } from './player-details/player-details.component';
 
 const routes: Routes = [
   { path: '', redirectTo: '', pathMatch: 'full' },
   { path: 'add-league', component: AddLeagueComponent },
   { path: 'add-result/:league-id', component: AddResultComponent },
+  { path: 'player/:player-id', component: PlayerDetailsComponent },
 ];
 
 @NgModule({
