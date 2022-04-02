fc42d1e8658927deb2a21dce44b6b265dae41927
CatBloom <shomail.cat@gmail.com>
2022-04-02 22:22:27 +0900

    Add:AngulerMaterial mat-tableを設置 #23
    sampleデータが入ってるため後に変更

--- src/app/shared/components/table/table.component.ts
+++ src/app/shared/components/table/table.component.ts
@@ -1,12 +1,65 @@
 import { Component, OnInit } from '@angular/core';
+import { animate, state, style, transition, trigger } from '@angular/animations';
 
 @Component({
   selector: 'app-table',
   templateUrl: './table.component.html',
   styleUrls: ['./table.component.scss'],
+  animations: [
+    trigger('detailExpand', [
+      state('collapsed', style({ height: '0px', minHeight: '0' })),
+      state('expanded', style({ height: '*' })),
+      transition('expanded <=> collapsed', animate('225ms cubic-bezier(0.4, 0.0, 0.2, 1)')),
+    ]),
+  ],
 })
 export class TableComponent implements OnInit {
+  dataSource = sampleData;
+  columnsToDisplay: string[] = ['rank', 'point', 'calcPoint', 'date'];
+  expandedElement?: ResultTable | null;
+
   constructor() {}
 
   ngOnInit(): void {}
 }
+
+interface ResultTable {
+  rank: string;
+  point: number;
+  calcPoint: number;
+  date: Date;
+  group: number;
+}
+interface ResultTableWrapper extends ResultTable {
+  resultData: ResultTable[];
+}
+
+// sampleデータ
+const sampleData: ResultTableWrapper[] = [
+  {
+    rank: '1位',
+    point: 40000,
+    calcPoint: 40,
+    date: new Date('Sat Apr 02 2022 22:00:00 GMT+0900 (日本標準時)'),
+    group: 1,
+    resultData: [
+      { rank: '1位', point: 40000, calcPoint: 40, date: new Date('2020/02/02'), group: 1 },
+      { rank: '2位', point: 30000, calcPoint: 30, date: new Date('2020/02/02'), group: 1 },
+      { rank: '3位', point: 20000, calcPoint: 20, date: new Date('2020/02/02'), group: 1 },
+      { rank: '4位', point: 10000, calcPoint: 10, date: new Date('2020/02/02'), group: 1 },
+    ],
+  },
+  {
+    rank: '2位',
+    point: 20000,
+    calcPoint: 30,
+    date: new Date('Sat Apr 02 2022 23:00:00 GMT+0900 (日本標準時)'),
+    group: 2,
+    resultData: [
+      { rank: '1位', point: 60000, calcPoint: 40, date: new Date('2020/02/03'), group: 2 },
+      { rank: '2位', point: 25000, calcPoint: 30, date: new Date('2020/02/03'), group: 2 },
+      { rank: '3位', point: 20000, calcPoint: 20, date: new Date('2020/02/03'), group: 2 },
+      { rank: '4位', point: -5000, calcPoint: 10, date: new Date('2020/02/03'), group: 2 },
+    ],
+  },
+];
