4f63961c3c295f089195400965edd25fd6d783ca
CatBloom <shomail.cat@gmail.com>
2022-04-04 20:24:38 +0900

    Add:chartJs pie-chartを設置 #23

--- src/app/shared/components/pie-chart/pie-chart.component.ts
+++ src/app/shared/components/pie-chart/pie-chart.component.ts
@@ -1,4 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
+import DatalabelsPlugin from 'chartjs-plugin-datalabels';
+import { ChartConfiguration, ChartData } from 'chart.js';
 
 @Component({
   selector: 'app-pie-chart',
@@ -6,7 +8,62 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./pie-chart.component.scss'],
 })
 export class PieChartComponent implements OnInit {
+  @Input('pieData') pieData!: number[];
+
+  // datasets
+  public pieChartData: ChartData<'pie', number[], string | string[]> = {
+    labels: ['1位', '2位', '3位', '4位'],
+    datasets: [
+      {
+        data: [],
+      },
+    ],
+  };
+
+  // options
+  public pieChartOptions: ChartConfiguration['options'] = {
+    responsive: true,
+    plugins: {
+      legend: {
+        display: false,
+        position: 'top',
+        onClick: function () {
+          return false;
+        },
+      },
+      tooltip: {
+        callbacks: {
+          label: (tooltipItem) => {
+            return tooltipItem.formattedValue + '回';
+          },
+        },
+      },
+      datalabels: {
+        font: {
+          weight: 'bold',
+          size: 15,
+        },
+        formatter: (value, ctx) => {
+          if (ctx.chart.data.labels) {
+            let label = ctx.chart.data.labels[ctx.dataIndex];
+            let sum = 0;
+            let dataArr = ctx.chart.data.datasets[0].data;
+            dataArr.map((data) => {
+              sum += Number(data);
+            });
+            let percentage: string = ':' + ((value * 100) / sum).toFixed(1) + '%';
+            return percentage ? label + percentage : label;
+          }
+        },
+      },
+    },
+  };
+
+  public pieChartPlugins = [DatalabelsPlugin];
+
   constructor() {}
 
-  ngOnInit(): void {}
+  ngOnInit(): void {
+    this.pieChartData.datasets = [{ data: this.pieData }];
+  }
 }
