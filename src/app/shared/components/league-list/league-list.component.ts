93ac201519809c583cc5856b8049b8b14e7ec87a
CatBloom <shomail.cat@gmail.com>
2022-04-17 16:23:54 +0900

    Add:leagueListを取得してリンク表示する #32
    SASSスタイルとリンクは設計詳細が決定後に修正予定

--- src/app/shared/components/league-list/league-list.component.ts
+++ src/app/shared/components/league-list/league-list.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-league-list',
@@ -6,6 +6,7 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./league-list.component.scss'],
 })
 export class LeagueListComponent implements OnInit {
+  @Input('leagueList') leagueList!: string[];
   constructor() {}
 
   ngOnInit(): void {}
