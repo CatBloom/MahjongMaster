84f647c40896b4b9db8a13c14180ffe2ebe78d3e
CatBloom <shomail.cat@gmail.com>
2022-04-20 00:34:05 +0900

    Change:iconをanguler-fontawesomeに変更 #37
    classに記入する方法からライブラリimportでバインドする方法に変更

--- src/app/shared/components/header/header.component.ts
+++ src/app/shared/components/header/header.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { faTwitter } from '@fortawesome/free-brands-svg-icons';
 
 @Component({
   selector: 'app-header',
@@ -6,6 +7,8 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./header.component.scss'],
 })
 export class HeaderComponent implements OnInit {
+  iconTwitter = faTwitter;
+
   constructor() {}
 
   ngOnInit(): void {}
--- src/app/shared/components/league-list/league-list.component.ts
+++ src/app/shared/components/league-list/league-list.component.ts
@@ -1,4 +1,5 @@
 import { Component, Input, OnInit } from '@angular/core';
+import { faSquareCaretRight } from '@fortawesome/free-solid-svg-icons';
 
 @Component({
   selector: 'app-league-list',
@@ -7,6 +8,9 @@ import { Component, Input, OnInit } from '@angular/core';
 })
 export class LeagueListComponent implements OnInit {
   @Input('leagueList') leagueList!: string[];
+
+  iconSquareCaretRight = faSquareCaretRight;
+
   constructor() {}
 
   ngOnInit(): void {}
