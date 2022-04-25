969d99ae819fd83fe1c118eb328cd015a4f4aa80
CatBloom <shomail.cat@gmail.com>
2022-04-26 00:58:06 +0900

    Add:iconがある時はアイコンを表示する #47

--- src/app/shared/components/button/button.component.ts
+++ src/app/shared/components/button/button.component.ts
@@ -1,4 +1,5 @@
 import { Component, Input, OnInit } from '@angular/core';
+import { IconDefinition } from '@fortawesome/free-solid-svg-icons';
 
 @Component({
   selector: 'app-button',
@@ -7,7 +8,7 @@ import { Component, Input, OnInit } from '@angular/core';
 })
 export class ButtonComponent implements OnInit {
   @Input() buttonText!: string;
-
+  @Input() icon?: IconDefinition;
   constructor() {}
 
   ngOnInit(): void {}
