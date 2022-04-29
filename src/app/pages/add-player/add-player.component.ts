4ffb707bf9407ec5597eba52bce2df774d161e71
CatBloom <shomail.cat@gmail.com>
2022-04-30 01:47:29 +0900

    Add:プレイヤー名Inputとボタンを追加 #59

--- src/app/pages/add-player/add-player.component.ts
+++ src/app/pages/add-player/add-player.component.ts
@@ -1,11 +1,15 @@
 import { Component, OnInit } from '@angular/core';
-
+import { FormControl, FormGroup, Validators } from '@angular/forms';
 @Component({
   selector: 'app-add-player',
   templateUrl: './add-player.component.html',
   styleUrls: ['./add-player.component.scss'],
 })
 export class AddPlayerComponent implements OnInit {
+  formGroup = new FormGroup({
+    playerName: new FormControl('', [Validators.required]),
+  });
+
   constructor() {}
 
   ngOnInit(): void {}
