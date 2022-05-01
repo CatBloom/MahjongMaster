2a006693df75c8bd36d04ed5a4f8ba2b04e118d2
CatBloom <shomail.cat@gmail.com>
2022-05-01 17:46:06 +0900

    Add:リストを表示し、ボタン切替用の関数を定義 #59

--- src/app/shared/components/player-list/player-list-edit/player-list-edit.component.ts
+++ src/app/shared/components/player-list/player-list-edit/player-list-edit.component.ts
@@ -1,4 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
+import { FormControl } from '@angular/forms';
+import { PlayerList } from '../../../../shared/interfaces/player';
 
 @Component({
   selector: 'app-player-list-edit',
@@ -6,7 +8,24 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./player-list-edit.component.scss'],
 })
 export class PlayerListEditComponent implements OnInit {
+  @Input() playerList!: PlayerList;
+  isPlayerEdit = false;
+  inputPlayer: FormControl = new FormControl();
+
   constructor() {}
 
   ngOnInit(): void {}
+
+  onEditClick() {
+    this.inputPlayer.setValue(this.playerList.playerName);
+    this.isPlayerEdit = true;
+  }
+
+  onSaveClick() {
+    if (this.inputPlayer.value !== this.playerList.playerName) {
+      console.log(this.playerList.playerId);
+      console.log(this.inputPlayer.value);
+    }
+    this.isPlayerEdit = false;
+  }
 }
