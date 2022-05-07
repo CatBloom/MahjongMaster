fcf46b8d4f71d901e41939a32e044f7f270679e9
CatBloom <shomail.cat@gmail.com>
2022-05-07 23:23:14 +0900

    Change:受け取るデータを変更 #62
    string配列からLeagueDataset配列に変更

--- src/app/pages/admin-my-league/admin-my-league.component.ts
+++ src/app/pages/admin-my-league/admin-my-league.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { faFileCirclePlus } from '@fortawesome/free-solid-svg-icons';
+import { LeagueDataset } from '../../shared/interfaces/league';
 
 @Component({
   selector: 'app-admin-my-league',
@@ -8,9 +9,25 @@ import { faFileCirclePlus } from '@fortawesome/free-solid-svg-icons';
 })
 export class AdminMyLeagueComponent implements OnInit {
   iconFileCirclePlus = faFileCirclePlus;
-  leagueList: string[] = ['sampleLeague1', 'sampleLeague2', 'sampleLeague3', 'sampleLeague4'];
+  leagueList: LeagueDataset[] = [];
 
   constructor() {}
 
-  ngOnInit(): void {}
+  ngOnInit(): void {
+    //sampleData
+    this.leagueList = [
+      {
+        leagueId: '01',
+        leagueName: 'catBloomLeague',
+      },
+      {
+        leagueId: '02',
+        leagueName: 'sampleLeague02',
+      },
+      {
+        leagueId: '03',
+        leagueName: 'sampleLeague03',
+      },
+    ];
+  }
 }
--- src/app/shared/interfaces/league.ts
+++ src/app/shared/interfaces/league.ts
@@ -1,5 +1,12 @@
 import { Rules } from '../interfaces/rules';
 
+export interface League {
+  leagueName: string;
+  leagueManual?: string;
+}
+export interface LeagueDataset extends League {
+  leagueId: string;
+}
 export interface LeagueDialog {
   leagueName: string;
   leagueManual: string;
