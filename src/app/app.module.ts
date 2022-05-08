dd4651bb91f0c39b0a0e7578bdd15591d555d19c
CatBloom <shomail.cat@gmail.com>
2022-05-08 23:17:38 +0900

    Add:DateTimePickerを追加 #68
    style.scssにプラグインのCSSを追加

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,6 +8,7 @@ import { HttpClientModule } from '@angular/common/http';
 import { MaterialModule } from './material.module';
 import { NgChartsModule } from 'ng2-charts';
 import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import { OwlDateTimeModule, OwlNativeDateTimeModule } from '@danielmoncada/angular-datetime-picker';
 
 // components
 import { AppComponent } from './app.component';
@@ -68,6 +69,8 @@ import { JapanesePipe } from './shared/pipes/japanese.pipe';
     MaterialModule,
     NgChartsModule,
     FontAwesomeModule,
+    OwlDateTimeModule,
+    OwlNativeDateTimeModule,
   ],
   providers: [],
   bootstrap: [AppComponent],
