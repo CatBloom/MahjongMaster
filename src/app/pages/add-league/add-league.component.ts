09240a49d21dcb34ef21e2521bc78b17c62245a0
CatBloom <shomail.cat@gmail.com>
2022-05-09 20:25:48 +0900

    Add:DateTimePickerを設置 #68 #69
    整形データを表示するため、inputを非表示で対応

--- src/app/pages/add-league/add-league.component.ts
+++ src/app/pages/add-league/add-league.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
 import { FormControl, FormGroup, Validators } from '@angular/forms';
 import { Rules } from '../../shared/interfaces/rules';
 import { LeagueDialog } from '../../shared/interfaces/league';
@@ -6,16 +6,21 @@ import { LeagueService } from '../../shared/services/league.service';
 import { RulesService } from '../../shared/services/rules.service';
 import { MatDialog } from '@angular/material/dialog';
 import { AddLeagueDialogComponent } from '../add-league/add-league-dialog/add-league-dialog.component';
+import { Subject } from 'rxjs';
+import { takeUntil } from 'rxjs/operators';
+import { DatePipe } from '@angular/common';
 
 @Component({
   selector: 'app-add-league',
   templateUrl: './add-league.component.html',
   styleUrls: ['./add-league.component.scss'],
 })
-export class AddLeagueComponent implements OnInit {
+export class AddLeagueComponent implements OnInit, OnDestroy {
   formGroup = new FormGroup({
     leagueName: new FormControl('', [Validators.required]),
     leagueManual: new FormControl('', []),
+    leagueDate: new FormControl('', []),
+    leagueDisplayDate: new FormControl('', []),
     rulesRadio: new FormControl('', [Validators.required]),
   });
 
@@ -40,9 +45,31 @@ export class AddLeagueComponent implements OnInit {
     inputUma4: 0,
   };
 
-  constructor(private leagueService: LeagueService, private rulesService: RulesService, private matDialog: MatDialog) {}
+  private onDestroy$ = new Subject();
 
-  ngOnInit(): void {}
+  constructor(
+    private leagueService: LeagueService,
+    private rulesService: RulesService,
+    private matDialog: MatDialog,
+    private datePipe: DatePipe
+  ) {}
+
+  ngOnInit(): void {
+    // 日付整形用
+    this.formGroup
+      .get('leagueDate')
+      ?.valueChanges.pipe(takeUntil(this.onDestroy$))
+      .subscribe(() => {
+        const leagueDate: Array<Date> = this.formGroup.get('leagueDate')?.value;
+        const leagueStartDate = this.datePipe.transform(leagueDate[0], 'yyyy/MM/dd HH:mm');
+        const leagueFinishDate = this.datePipe.transform(leagueDate[1], 'yyyy/MM/dd HH:mm');
+        this.formGroup.get('leagueDisplayDate')?.setValue(`${leagueStartDate}~${leagueFinishDate}`);
+      });
+  }
+
+  ngOnDestroy(): void {
+    this.onDestroy$.next();
+  }
 
   setRules(rules: Rules) {
     this.rules = rules;
