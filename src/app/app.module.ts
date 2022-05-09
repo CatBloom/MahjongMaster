4464873ae5adce1c5a1e89b205fb9d34aa9f5b4f
CatBloom <shomail.cat@gmail.com>
2022-05-09 21:32:22 +0900

    Change:パスの修正 #27

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -20,7 +20,7 @@ import { RulesComponent } from './pages/shared/components/rules/rules.component'
 import { RuleListComponent } from './pages/shared/components/rule-list/rule-list.component';
 import { PlayerDetailsComponent } from './pages/player-details/player-details.component';
 import { TableComponent } from './pages/shared/components/table/table.component';
-import { PieChartComponent } from './shared/components/pie-chart/pie-chart.component';
+import { PieChartComponent } from './pages/shared/components/pie-chart/pie-chart.component';
 import { LineChartComponent } from './shared/components/line-chart/line-chart.component';
 import { AdminMyLeagueComponent } from './pages/admin-my-league/admin-my-league.component';
 import { LeagueListComponent } from './pages/shared/components/league-list/league-list.component';
