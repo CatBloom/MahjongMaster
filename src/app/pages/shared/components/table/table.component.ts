c9750f0dad6b7cebf5d15a5e665465bb9100c90b
CatBloom <shomail.cat@gmail.com>
2022-05-11 00:47:37 +0900

    Delete:expandedのlogicを削除 #74

--- src/app/pages/shared/components/table/table.component.ts
+++ src/app/pages/shared/components/table/table.component.ts
@@ -1,26 +1,17 @@
 import { Component, Input, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
-import { animate, state, style, transition, trigger } from '@angular/animations';
-import { PlayerResult, PlayerResultWrapper, LeagueResult } from '../../../../shared/interfaces/result';
+import { PlayerResultWrapper, LeagueResult } from '../../../../shared/interfaces/result';
 
 @Component({
   selector: 'app-table',
   templateUrl: './table.component.html',
   styleUrls: ['./table.component.scss'],
-  animations: [
-    trigger('detailExpand', [
-      state('collapsed', style({ height: '0px', minHeight: '0' })),
-      state('expanded', style({ height: '*' })),
-      transition('expanded <=> collapsed', animate('225ms cubic-bezier(0.4, 0.0, 0.2, 1)')),
-    ]),
-  ],
 })
 export class TableComponent implements OnInit {
   @Input() columns!: string[];
   @Input() results!: PlayerResultWrapper[] | LeagueResult[];
   dataSource = [{}];
   columnsToDisplay: string[] = [];
-  expandedElement?: PlayerResult | null;
 
   constructor(private router: Router) {}
 
