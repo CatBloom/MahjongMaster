334f2c5f90cf6503faee0ff6a5e983455ba86bd1
CatBloom <shomail.cat@gmail.com>
2022-06-12 22:58:01 +0900

    Add:非同期処理中にスピナーを表示 #120
    app.componentのmainに被せる

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,17 +1,28 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, AfterContentChecked, ChangeDetectorRef } from '@angular/core';
 import { AuthService } from './shared/auth/auth.service';
+import { SpinnerService } from './shared/services/spinner.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss'],
 })
-export class AppComponent implements OnInit {
+export class AppComponent implements OnInit, AfterContentChecked {
   title = 'title';
+  isSpinner = this.spinner.isSpinner$;
 
-  constructor(private authService: AuthService) {}
+  constructor(
+    private authService: AuthService,
+    private spinner: SpinnerService,
+    private changeDetectorRef: ChangeDetectorRef
+  ) {}
 
   ngOnInit(): void {
     this.authService.getAuthState();
+  }
+
+  // Error Ng100の対策
+  ngAfterContentChecked() {
+    this.changeDetectorRef.detectChanges();
   }
 }
