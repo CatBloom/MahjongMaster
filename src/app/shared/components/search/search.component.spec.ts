436de3b06127e98e4d5e778414c5145f9408d426
CatBloom <shomail.cat@gmail.com>
2022-08-09 01:29:32 +0900

    Add:検索用のinputを作成 #141
    親コンポーネントでinput値を受け取る

--- src/app/shared/components/search/search.component.spec.ts
+++ src/app/shared/components/search/search.component.spec.ts
@@ -1,5 +1,4 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
-
 import { SearchComponent } from './search.component';
 
 describe('SearchComponent', () => {
@@ -20,5 +19,12 @@ describe('SearchComponent', () => {
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('@Output test', () => {
+    component.search.setValue('test');
+    component.sendValue.subscribe((value) => {
+      expect(value).toBe('test');
+    });
   });
 });
